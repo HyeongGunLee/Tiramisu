7747df8cd1e2ed9e1b58f093055709661b15cffb
jjharu <kukaro4515@gmai.com>
2017-07-17 21:09:22 +0900

    add DoLogin

--- TiramisuProject/Tiramisu/src/work/controller/FrontController.java
+++ TiramisuProject/Tiramisu/src/work/controller/FrontController.java
@@ -5,27 +5,56 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class FrontController
  */
 public class FrontController extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private MemberDao dao = new MemberDao();
-       
-    public FrontController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-    protected void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-    
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		process(request,response);
+	protected void process(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String action = request.getParameter("action");
+		System.out.println("Debug : Process 진입");
+		switch (action) {
+		case "login":
+			doLogin(request, response);
+			break;
+		case "logout":
+			break;
+		case "signUp":
+			break;
+		case "editPersonalDate":
+			break;
+		default:
+			break;
+		}
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doLogin(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String memberId = request.getParameter("memberId");
+		String memberPw = request.getParameter("memberPw");
+		if(memberId.length() > 0 && memberPw.length() >0){
+			HttpSession sess = request.getSession();
+			sess.setAttribute("memberId", memberId);
+			System.out.println("Debug : sess.isNew() : " + sess.isNew());
+			System.out.println("Debug : sess.getId() : " + sess.getId());
+		}
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		process(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		process(request, response);
 	}
 
